ddced34531f9d3fa207788fe64f21d268a826f73
Sarwar Hossain <sarwarhossain242@outlook.com>
2024-08-22 17:55:29 +0600

    Initial Commit

---
+++ src/app/components/user-list/user-list.component.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+import { IUserModel } from 'src/app/models/UserModel';
+
+@Component({
+  selector: 'app-user-list',
+  templateUrl: './user-list.component.html',
+  styleUrls: ['./user-list.component.css']
+})
+export class UserListComponent {
+  title:string="All Users List";
+  userData:IUserModel={
+      serial: 4,
+      user_id: "leads",
+      full_name: "LEADS Corporation Limited",
+      email: "hr@leads.com",
+      mobile_number: "01234567890",
+      present_address: "LEADS Tower, Mirpur 14",
+      permanent_address: "LEADS Tower, Mirpur 14"
+  }
+}
---
+++ src/app/models/UserModel.ts
@@ -0,0 +1,10 @@
+export interface IUserModel{    
+    serial ?: number;
+    user_id ?: string;
+    full_name?: string;
+    email?: string;
+    mobile_number?: string;
+    present_address?: string;
+    permanent_address?: string;
+}
+
